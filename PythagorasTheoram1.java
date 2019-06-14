6d2c8f0c1d9f1eb62494a670f5366e2b15af8a10
Reza Akhlaqi <36783229+rzjrh@users.noreply.github.com>
2019-06-14 03:09:06 -0500

    Add files via upload

---
+++ PythagorasTheoram1.java
@@ -0,0 +1,15 @@
+
+//Reza Akhlaqi
+//use import java.lang.Math
+
+
+
+package pythagorastheoram1;
+import java.lang.Math;
+public class PythagorasTheoram1 {
+    public static void main(String[] args) {
+Pythagorean calc = new Pythagorean();
+
+System.out.println(calc.CalcHyp(20.1, 11.14));    }
+    
+}
---
+++ Pythagorean.java
@@ -0,0 +1,12 @@
+
+package pythagorastheoram1;
+
+public class Pythagorean {
+    
+       public double CalcHyp (double legA, double legB) {
+        double legC = (legA*legA + legB*legB);
+        double squareroot = Math.sqrt(legC);
+        return squareroot;
+    } 
+    
+}
